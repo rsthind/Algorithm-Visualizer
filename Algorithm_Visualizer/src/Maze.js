0a61c1f19f3e09065baa2cfc28fb9940cd2724ae
Ranveer Thind <ranveerthind01@hotmail.com>
2020-03-18 14:23:04 -0700

    Initial Base Maze Creation

--- Algorithm_Visualizer/src/Maze.js
+++ Algorithm_Visualizer/src/Maze.js
@@ -1,7 +1,18 @@
 var canvas;
-var ctx;
+var context;
 
-function intit() {
-    canvas = document.getElementById("myCanvas");
-
+function draw() {
+    context.fillStyle = '#FF0000' //red
+    context.beginPath();
+    context.rect(10, 10, 50, 50);
+    context.closePath();
+    context.fill();
 }
+
+function init() {
+    canvas = document.getElementById("myCanvas");
+    context = canvas.getContext("2d");
+    draw();
+}
+
+init();
