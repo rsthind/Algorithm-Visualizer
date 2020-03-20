3dae6626d943ebf70c2a3c7a61ed40eb9118efe8
Ranveer Thind <ranveerthind01@hotmail.com>
2020-03-19 21:15:51 -0700

    Defining Constants

--- path-visualizer/src/AlgorithmVisualizer/AlgorithmVisualizer.jsx
+++ path-visualizer/src/AlgorithmVisualizer/AlgorithmVisualizer.jsx
@@ -76,3 +76,39 @@ export default class AlgorithmVisualizer extends Component {
         );
     }
 }
+
+const getInitialGrid = () => {
+    const grid = [];
+    for (let row = 0; row < 20; row++) {
+        const currentRow = [];
+        for (let col = 0; col < 50; col++) {
+            currentRow.push(createNode(col, row));
+        }
+        grid.push(currentRow);
+    }
+    return grid;
+};
+
+const createNode = (col, row) => {
+    return {
+        col,
+        row,
+        isStart: row === START_NODE_ROW && col === START_NODE_COL,
+        isFinish: row === FINISH_NODE_ROW && col === FINISH_NODE_COL,
+        distance: Infinity,
+        isVisited: false,
+        isWall: false,
+        previousNode: null,
+    };
+};
+
+const getNewGridWithWallToggled = (grid, row, col) => {
+    const newGrid = grid.slice();
+    const node = newGrid[row][col];
+    const newNode = {
+        ...node,
+        isWall: !node.isWall,
+    };
+    newGrid[row][col] = newNode;
+    return newGrid;
+}
